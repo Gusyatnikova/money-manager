8ffe07c06b0d55f270a536e65d0f6cc247c496cf
Gusyatnikova@github.com <crysnat08@gmail.com>
2022-11-06 16:57:23 +0300

    refactor(http): rename funds to money

--- money-manager/delivery/http/http_v1/balance.go
+++ money-manager/delivery/http/http_v1/balance.go
@@ -2,7 +2,7 @@ package http_v1
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/pkg/errors"
+	"money-manager/money-manager/delivery"
 	"net/http"
 
 	"money-manager/money-manager/entity"
@@ -29,24 +29,21 @@ type fundsReqBody struct {
 	Money  money  `json:"funds"`
 }
 
-type transferFundsReqBody struct {
+type transferMoneyReqBody struct {
 	FromUserId string `json:"from_user_id"`
 	ToUserId   string `json:"to_user_id"`
 	Money      money  `json:"funds"`
 }
 
-type transferFundsResBody struct {
+type transferMoneyResBody struct {
 	FromUser userBalanceResp `json:"sender"`
 	ToUser   userBalanceResp `json:"recipient"`
 }
 
-const UserIdParam = "user_id"
-
-var ErrBadContentType = errors.New("Content-Type application/json is missing")
-var ErrBadRequestBody = errors.New("Request body is incorrect")
+const UserIdParamName = "user_id"
 
 func (e *ServerHandler) GetBalance(eCtx echo.Context) error {
-	usr := eCtx.QueryParam(UserIdParam)
+	usr := eCtx.QueryParam(UserIdParamName)
 
 	bal, err := e.uc.GetBalance(eCtx.Request().Context(), entity.UserId(usr))
 	if err != nil {
@@ -56,45 +53,45 @@ func (e *ServerHandler) GetBalance(eCtx echo.Context) error {
 	return eCtx.JSON(http.StatusOK, makeUserBalanceResponse(entity.UserId(usr), bal))
 }
 
-func (e *ServerHandler) AddFunds(eCtx echo.Context) error {
+func (e *ServerHandler) AddMoney(eCtx echo.Context) error {
 	reqBody, err := parseUserMoneyBody(eCtx)
 	if err != nil {
 		return e.noContentErrResponse(eCtx, err)
 	}
 
-	err = e.uc.AddFundsToUser(eCtx.Request().Context(), entity.UserId(reqBody.UserId), reqBody.Money.Value, reqBody.Money.Unit)
+	err = e.uc.AddMoneyToUser(eCtx.Request().Context(), entity.UserId(reqBody.UserId), reqBody.Money.Value, reqBody.Money.Unit)
 	if err != nil {
 		return e.noContentErrResponse(eCtx, err)
 	}
 
-	eCtx.QueryParams().Set(UserIdParam, reqBody.UserId)
+	eCtx.QueryParams().Set(UserIdParamName, reqBody.UserId)
 
 	return e.GetBalance(eCtx)
 }
 
-func (e *ServerHandler) DebitFunds(eCtx echo.Context) error {
+func (e *ServerHandler) DebitMoney(eCtx echo.Context) error {
 	reqBody, err := parseUserMoneyBody(eCtx)
 	if err != nil {
 		return e.noContentErrResponse(eCtx, err)
 	}
 
-	err = e.uc.DebitFunds(eCtx.Request().Context(), entity.UserId(reqBody.UserId), reqBody.Money.Value, reqBody.Money.Unit)
+	err = e.uc.DebitMoney(eCtx.Request().Context(), entity.UserId(reqBody.UserId), reqBody.Money.Value, reqBody.Money.Unit)
 	if err != nil {
 		return e.noContentErrResponse(eCtx, err)
 	}
 
-	eCtx.QueryParams().Set(UserIdParam, reqBody.UserId)
+	eCtx.QueryParams().Set(UserIdParamName, reqBody.UserId)
 
 	return e.GetBalance(eCtx)
 }
 
-func (e *ServerHandler) TransferFunds(eCtx echo.Context) error {
+func (e *ServerHandler) TransferMoney(eCtx echo.Context) error {
 	reqBody, err := parseUserTransferReqBody(eCtx)
 	if err != nil {
 		return e.noContentErrResponse(eCtx, err)
 	}
 
-	err = e.uc.TransferFundsUserToUser(
+	err = e.uc.TransferMoneyUserToUser(
 		eCtx.Request().Context(),
 		entity.UserId(reqBody.FromUserId),
 		entity.UserId(reqBody.ToUserId),
@@ -116,34 +113,34 @@ func (e *ServerHandler) TransferFunds(eCtx echo.Context) error {
 		return e.noContentErrResponse(eCtx, err)
 	}
 
-	return eCtx.JSON(http.StatusOK, makeTransferFundsResBody(userIdFrom, userIdTo, userBalFrom, userBalTo))
+	return eCtx.JSON(http.StatusOK, makeTransferMoneyResBody(userIdFrom, userIdTo, userBalFrom, userBalTo))
 }
 
 func parseUserMoneyBody(eCtx echo.Context) (fundsReqBody, error) {
 	frBody := fundsReqBody{}
 
 	if !isRequestBodyIsJSON(eCtx) {
-		return frBody, ErrBadContentType
+		return frBody, delivery.ErrBadContentType
 	}
 
 	err := eCtx.Bind(&frBody)
 	if err != nil {
-		return frBody, ErrBadRequestBody
+		return frBody, delivery.ErrBadRequestBody
 	}
 
 	return frBody, nil
 }
 
-func parseUserTransferReqBody(eCtx echo.Context) (transferFundsReqBody, error) {
-	frBody := transferFundsReqBody{}
+func parseUserTransferReqBody(eCtx echo.Context) (transferMoneyReqBody, error) {
+	frBody := transferMoneyReqBody{}
 
 	if !isRequestBodyIsJSON(eCtx) {
-		return frBody, ErrBadContentType
+		return frBody, delivery.ErrBadContentType
 	}
 
 	err := eCtx.Bind(&frBody)
 	if err != nil {
-		return frBody, ErrBadRequestBody
+		return frBody, delivery.ErrBadRequestBody
 	}
 
 	return frBody, nil
@@ -160,8 +157,8 @@ func makeUserBalanceResponse(usr entity.UserId, bal entity.Balance) userBalanceR
 	}
 }
 
-func makeTransferFundsResBody(usrFrom entity.UserId, usrTo entity.UserId, balFrom entity.Balance, balTo entity.Balance) transferFundsResBody {
-	return transferFundsResBody{
+func makeTransferMoneyResBody(usrFrom entity.UserId, usrTo entity.UserId, balFrom entity.Balance, balTo entity.Balance) transferMoneyResBody {
+	return transferMoneyResBody{
 		FromUser: makeUserBalanceResponse(usrFrom, balFrom),
 		ToUser:   makeUserBalanceResponse(usrTo, balTo),
 	}
