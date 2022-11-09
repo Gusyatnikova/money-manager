ebd17f98753a70efb1cc651e9622cd8a61e8fbf5
Gusyatnikova@github.com <crysnat08@gmail.com>
2022-11-09 18:17:06 +0300

    refactor(core):

--- money-manager/usecase/errors.go
+++ money-manager/usecase/errors.go
@@ -3,19 +3,11 @@ package usecase
 import "github.com/pkg/errors"
 
 var ErrNotFound = errors.New("Requested resource is not found")
-
 var ErrInternalError = errors.New("Internal server error")
-
 var ErrDuplication = errors.New("Resource is already exists")
-
 var ErrInvalidUser = errors.New("Invalid user")
-
 var ErrInvalidMoney = errors.New("Invalid money")
-
 var ErrMoneyLimitIsExceeded = errors.New("Money limit is exceeded")
-
 var ErrNotEnoughMoney = errors.New("Insufficient funds to complete the operation")
-
 var ErrInvalidReserve = errors.New("Invalid reserve")
-
 var ErrInvalidReportInfo = errors.New("Invalid request for report")
